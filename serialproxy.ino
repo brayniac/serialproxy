806cece8a6a0e30c877c71ab4fffeb5e2f08a88a
brayniac <brayniac@gmail.com>
2014-09-22 22:19:43 -0700

    initial commit

---
+++ serialproxy.ino
@@ -0,0 +1,16 @@
+void setup()
+{
+  pinMode(0,INPUT);
+  pinMode(1,INPUT);
+  pinMode(13,OUTPUT);
+  pinMode(8,OUTPUT);
+}
+void loop()
+{
+  digitalWrite(13,HIGH);
+  digitalWrite(8,HIGH);
+  delay(500);
+  digitalWrite(13,LOW);
+  digitalWrite(8,LOW);
+  delay(500);
+}
